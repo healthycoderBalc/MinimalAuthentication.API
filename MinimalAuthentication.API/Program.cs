4349912b9dbfb61b94cd7401a99ca648b34efdb5
Maria Romero <mmrbalcazar@gmail.com>
2024-06-27 09:39:12 -0300

    Implementing JWT Authentication

--- MinimalAuthentication.API/Program.cs
+++ MinimalAuthentication.API/Program.cs
@@ -1,5 +1,26 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWTToken:Issuer"],
+            ValidAudience = builder.Configuration["JWTToken:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Convert.FromBase64String(builder.Configuration["JWTToken:Key"]))
+        };
+    });
+
+
 var app = builder.Build();
+
+app.UseAuthentication();
 
 app.MapGet("/", () => "Hello World!");
 
